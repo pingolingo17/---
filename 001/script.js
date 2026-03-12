3aa31c02b7a612078a5737a706381f9f42c47d66
pingolingo17 <bielulhoa1705@gmail.com>
2026-03-12 16:16:11 -0300

    Add files via upload

---
+++ 001/script.js
@@ -0,0 +1,246 @@
+
+function login() {
+  const nome1 = document.getElementById("nome1").value
+    .trim()
+    .toLowerCase();
+
+  const nome2 = document.getElementById("nome2").value
+    .trim()
+    .toLowerCase();
+
+
+
+  const mensagem = document.getElementById("mensagem");
+
+  if (nome1 === "alycia" && nome2 === "ummai") {
+    window.location.href = "00110001-00111001-00110110-00110111.html";
+  } else {
+    mensagem.innerText = "Credenciais incorretas.";
+  }
+}
+
+function enigma1() {
+
+  const resposta001 = document.getElementById("resposta001").value
+  ;
+
+
+  if (resposta001 === "1967") {
+    window.location.href = "rec1.html"
+  } else {
+    mensagem.innerText = "Resposta incorreta.";
+  }
+}
+
+function enigma2() {
+
+  const resposta002 = document.getElementById("resposta002").value
+  ;
+
+
+  if (resposta002 === "29") {
+    window.location.href = "rec2.html"
+  } else {
+    mensagem.innerText = "Resposta incorreta.";
+  }
+}
+
+function enigma3() {
+
+  const resposta003 = document.getElementById("resposta003").value
+    .trim()
+    .toLowerCase();
+  ;
+
+
+  if (resposta003 === "setembro") {
+    window.location.href = "rec3.html"
+  } else {
+    mensagem.innerText = "Resposta incorreta.";
+  }
+}
+
+function enigma4() {
+
+  const resposta004 = document.getElementById("resposta004").value
+  ;
+
+
+  if (resposta004 === "22:18") {
+    window.location.href = "rec4.html"
+  } else {
+    mensagem.innerText = "Resposta incorreta.";
+  }
+}
+
+function enigma5() {
+
+  const resposta005 = document.getElementById("resposta005").value
+  ;
+
+
+  if (resposta005 === "33") {
+    window.location.href = "rec5.html"
+  } else {
+    mensagem.innerText = "Resposta incorreta.";
+  }
+}
+
+function enigma6() {
+
+  const resposta006 = document.getElementById("resposta006").value
+    .trim()
+    .toLowerCase();
+  ;
+
+
+  if (resposta006 === "bonfire") {
+    window.location.href = "rec6.html"
+  } else {
+    mensagem.innerText = "Resposta incorreta.";
+  }
+}
+
+function enigma7() {
+
+  const resposta007 = document.getElementById("resposta007").value
+    .trim()
+    .toLowerCase();
+  ;
+
+
+  if (resposta007 === "crianca" || resposta007 === "criança") {
+    window.location.href = "rec7.html"
+  } else {
+    mensagem.innerText = "Resposta incorreta.";
+  }
+}
+
+function enigma8() {
+
+  const resposta008 = document.getElementById("resposta008").value
+    .trim()
+    .toLowerCase();
+  ;
+
+
+  if (resposta008 === "naicho") {
+    window.location.href = "rec8.html"
+  } else {
+    mensagem.innerText = "Resposta incorreta.";
+  }
+}
+
+function enigma9() {
+
+  const resposta009 = document.getElementById("resposta009").value
+    .trim()
+    .toLowerCase();
+  ;
+
+
+  if (resposta009 === "ultimo" || resposta009 === "último") {
+    window.location.href = "rec9.html"
+  } else {
+    mensagem.innerText = "Resposta incorreta.";
+  }
+}
+
+function enigma10() {
+
+  const resposta0010 = document.getElementById("resposta0010").value
+    .trim()
+    .toLowerCase();
+  ;
+
+
+  if (resposta0010 === "criptografia") {
+    window.location.href = "rec10.html"
+  } else {
+    mensagem.innerText = "Resposta incorreta.";
+  }
+}
+
+function enigma11() {
+
+  const resposta0011 = document.getElementById("resposta0011").value
+    .trim()
+    .toLowerCase();
+  ;
+
+
+  if (resposta0011 === "centauro" || resposta0011 === "centaurus") {
+    window.location.href = "rec11.html"
+  } else {
+    mensagem.innerText = "Resposta incorreta.";
+  }
+}
+
+function enigma12() {
+
+  const resposta0012 = document.getElementById("resposta0012").value
+    .trim()
+    .toLowerCase();
+  ;
+
+  if (resposta0012 === "pérola" || resposta0012 === "perola") {
+    window.location.href = "rec12.html"
+  } else {
+    mensagem.innerText = "Resposta incorreta.";
+  }
+}
+
+function enigma13() {
+
+
+  if (resposta0013 === "criptografia") {
+    window.location.href = "rec13.html"
+  } else {
+    mensagem.innerText = "Resposta incorreta.";
+  }
+}
+
+function enigma13() {
+
+  const resposta0013 = document.getElementById("resposta0013").value
+    .trim()
+    .toLowerCase();
+  ;
+
+
+  if (resposta0013 === "criptografia") {
+    window.location.href = "rec13.html"
+  } else {
+    mensagem.innerText = "Resposta incorreta.";
+  }
+}
+
+function enigma14() {
+
+  const resposta0014 = document.getElementById("resposta0014").value
+    .trim()
+    .toLowerCase();
+  ;
+
+
+  if (resposta0014 === "tudo") {
+    window.location.href = "rec14.html"
+  } else {
+    mensagem.innerText = "Resposta incorreta.";
+  }
+}
+
+function enigma15() {
+
+  const resposta0015 = document.getElementById("resposta0015").value
+    .trim()
+    .toLowerCase();
+  ;
+
+
+  if (resposta0015 === "camila ayumi") {
+    window.location.href = "rec15.html"
+  } else {
+    mensagem.innerText = "Resposta incorreta.";
+  }
+}
